425c52219df0c82e67e5a5084be551f5aea371df
XinyuP <xinyup@umich.edu>
2022-12-27 09:07:51 -0500

    add sign in link to nav bar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import { Routes, Route, Outlet } from 'react-router-dom';
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
+import SignIn from './routes/sign-in/sign-in.component';
 
 const Shop = () => {
-  return <h1>shopeeee</h1>
-}
+	return <h1>shopeeee</h1>;
+};
 
 const App = () => {
 	return (
 		<Routes>
 			<Route path='/' element={<Navigation />}>
-        <Route index element={<Home />} />
-        <Route path='shop' element={<Shop />} />
-        
+				<Route index element={<Home />} />
+				<Route path='shop' element={<Shop />} />
+				<Route path='sign-in' element={<SignIn />} />
 			</Route>
 		</Routes>
 	);
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -13,8 +13,10 @@ const Navigation = () => {
 				
 				<div className='nav-links-container'>
                     <Link className='nav-link' to='/shop'>
-                        SHOPPPP
-                        
+                        SHOP      
+                    </Link>
+                    <Link className='nav-link' to='/sign-in'>
+                        SIGN IN
                     </Link>
 				</div>
 			</div>
