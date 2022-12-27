016b80e52a7a55438530cc2f667b619a4c713134
XinyuP <xinyup@umich.edu>
2022-12-27 11:21:19 -0500

    comment out sign in with redirect

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,23 +1,23 @@
-import { useEffect } from 'react';
-import { getRedirectResult } from 'firebase/auth';
+// import { useEffect } from 'react';
+// import { getRedirectResult } from 'firebase/auth';
 
 import {
 	auth,
 	signInWithGooglePopup,
-	signInWithGoogleRedirect,
+	// signInWithGoogleRedirect,
 	createUserDocumentFromAuth,
 } from '../../utils/firebase/firebase.utils';
 
 const SignIn = () => {
-	useEffect(
-		() => async () => {
-			const response = await getRedirectResult(auth);
-			if (response) {
-				const userDocRef = await createUserDocumentFromAuth(response.user);
-			}
-		},
-		[] // pass [] here means run the callback function once when SignIn component mounts for the first time
-	); 
+	// useEffect(
+	// 	() => async () => {
+	// 		const response = await getRedirectResult(auth);
+	// 		if (response) {
+	// 			const userDocRef = await createUserDocumentFromAuth(response.user);
+	// 		}
+	// 	},
+	// 	[] 
+	// ); 
 
 	const logGooglePopupUser = async () => {
 		// whenever you make call to some database, this is going to be async
@@ -42,9 +42,9 @@ const SignIn = () => {
 		<div>
 			<h1>Sign IN</h1>
 			<button onClick={logGooglePopupUser}>Sign in with Google Popup</button>
-			<button onClick={signInWithGoogleRedirect}>
+			{/* <button onClick={signInWithGoogleRedirect}>
 				Sign in with Google Redirect
-			</button>
+			</button> */}
 		</div>
 	);
 };
