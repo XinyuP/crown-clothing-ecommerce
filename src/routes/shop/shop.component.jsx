43aa69b3150b5babf76a3bd3cd7044a042f4b247
XinyuP <xinyup@umich.edu>
2023-01-09 00:15:07 -0500

    nested Route in shop page

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,21 +1,21 @@
-import { useContext, Fragment } from 'react';
-import { CategoriesContext } from '../../contexts/categories.context';
-// import ProductCard from '../../components/product-card/product-card.component';
-import CategoryPreview from '../../components/category-preview/category-preview.component';
+import { Routes, Route } from 'react-router-dom';
+import CategoriesPreview from '../categories-preview/categories-preview.component';
+import Category from '../category/category.component';
 import './shop.styles.scss';
 
 const Shop = () => {
-	const { categoriesMap } = useContext(CategoriesContext);
 	return (
-		<div className='shop-container'>
-			{Object.keys(categoriesMap).map((title) => {
-				const products = categoriesMap[title];
-				return (
-					<CategoryPreview key={title} title={title} products={products} />
-				);
-			})}
-		</div>
+		<Routes>
+			<Route index element={<CategoriesPreview />} />
+			<Route path=':category' element={<Category />} />
+
+			<Route />
+			<Route />
+			<Route />
+			<Route />
+		</Routes>
 	);
 };
 
 export default Shop;
+// this is a collection preview
